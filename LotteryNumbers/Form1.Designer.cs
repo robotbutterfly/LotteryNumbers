142a483959db8f8fe59377ffe21102c22c634887
Tony Theodoropoulos <theodot@outlook.com>
2022-05-04 14:11:19 -0400

    Add project files.

---
+++ LotteryNumbers/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace LotteryNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-5, -1);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(421, 52);
+            this.titleLabel.TabIndex = 10;
+            this.titleLabel.Text = "Lottery Number Generator";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.Black;
+            this.outputLabel.Location = new System.Drawing.Point(56, 222);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(301, 49);
+            this.outputLabel.TabIndex = 9;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calcButton
+            // 
+            this.calcButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.YellowGreen;
+            this.calcButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calcButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcButton.ForeColor = System.Drawing.Color.White;
+            this.calcButton.Location = new System.Drawing.Point(153, 145);
+            this.calcButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(99, 37);
+            this.calcButton.TabIndex = 8;
+            this.calcButton.Text = "Generate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.ForeColor = System.Drawing.Color.White;
+            this.infoLabel.Location = new System.Drawing.Point(56, 66);
+            this.infoLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(307, 53);
+            this.infoLabel.TabIndex = 6;
+            this.infoLabel.Text = "Press the Button below to generate your winning 6/49 lottery numbers";
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(411, 307);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.infoLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Golden Bears Lottery";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ LotteryNumbers/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotteryNumbers
+{
+    public partial class Form1 : Form
+    {
+        int mode = 2; // 1 for no array, 2 with array
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            if (mode == 1)
+            {
+                noList();
+            }
+            else
+            {
+                withList();
+            }
+
+        }
+
+        public void noList()
+        {
+            const int LIMIT = 50;
+
+            int num1, num2, num3, num4, num5, num6;
+
+            Random randGen = new Random();
+
+            //get first value
+            num1 = randGen.Next(1, LIMIT);
+
+            //get second value and if not unique keep getting
+            num2 = randGen.Next(1, LIMIT);
+
+            while (num2 == num1)
+            {
+                num2 = randGen.Next(1, LIMIT);
+            }
+
+            //get third value and if not unique keep getting
+            num3 = randGen.Next(1, LIMIT);
+
+            while (num3 == num1 || num3 == num2)
+            {
+                num3 = randGen.Next(1, LIMIT);
+            }
+
+            //get fourth value and if not unique keep getting
+            num4 = randGen.Next(1, LIMIT);
+
+            while (num4 == num1 || num4 == num2 || num4 == num3)
+            {
+                num4 = randGen.Next(1, LIMIT);
+            }
+
+            //get fifth value and if not unique keep getting
+            num5 = randGen.Next(1, LIMIT);
+
+            while (num5 == num1 || num5 == num2 || num5 == num3 || num5 == num4)
+            {
+                num5 = randGen.Next(1, LIMIT);
+            }
+
+            //get sixth value and if not unique keep getting
+            num6 = randGen.Next(1, LIMIT);
+
+            while (num6 == num1 || num6 == num2 || num6 == num3 || num6 == num4 || num6 == num5)
+            {
+                num6 = randGen.Next(1, LIMIT);
+            }
+
+            outputLabel.Text = "Your winning numbers: \n" +
+                num1 + " " + num2 + " " + num3 + " " + num4 + " " + num5 + " " + num6;
+        }
+
+        public void withList()
+        {
+            const int LIMIT = 50; // LIMIT must be one more than the possible top value
+
+            List<int> numbers = new List<int>();
+            Random randGen = new Random();
+            int newNum;
+
+            //add the first value to the list
+            numbers.Add(randGen.Next(1, LIMIT));
+
+            //get values 2 to 6
+            for (int i = 2; i <= 6; i++)
+            {
+                newNum = randGen.Next(1, LIMIT);
+
+                //keep getting a new value until one is found that is unique
+                while (numbers.Contains(newNum))
+                {
+                    newNum = randGen.Next(1, LIMIT);
+                }
+
+                //add new unique value to the list
+                numbers.Add(newNum);
+            }
+
+            //sort the list and prep the output are for new output
+            numbers.Sort();
+            outputLabel.Text = "";
+
+            //display winning numbers
+            foreach(int i in numbers)
+            {
+                outputLabel.Text = outputLabel.Text + " " + i;
+            }
+        }
+    }
+}
